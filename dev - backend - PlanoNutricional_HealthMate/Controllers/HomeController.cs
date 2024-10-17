34068f9a84b4e88bf5101f9aa80d63794b1b355d
BarbaraFSilva7 <barbara.fariaspessoal@gmail.com>
2024-10-17 16:23:42 -0400

    Adicionar arquivos de projeto.

---
+++ dev - backend - PlanoNutricional_HealthMate/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+using dev___backend___PlanoNutricional_HealthMate.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace dev___backend___PlanoNutricional_HealthMate.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ dev - backend - PlanoNutricional_HealthMate/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace dev___backend___PlanoNutricional_HealthMate.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
